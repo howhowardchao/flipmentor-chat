ac49dd142d0fd2ccb917f4427866bd98784b1067
howhoward <howhoward.chao@gmail.com>
2024-12-13 16:58:15 +0800

    2412131637 - 更新部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
+  server: {
+    port: 3000,
+    host: true
+  }
 }) 
